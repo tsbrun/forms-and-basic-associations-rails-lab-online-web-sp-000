9d0bd6456054946c83b55bfc8b1b0d8545e941e8
TSephanya Brun <tsbrun@icloud.com>
2020-12-02 14:59:57 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -47,7 +47,8 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title)
+    params.require(:song).permit(:title, :artist_name, :genre_id, song_notes: [])
+    # will return unpermitted param, if you don't specify a param is meant to be an array
   end
 end
 
