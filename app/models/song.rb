e6a903b303e2793cc15fd3dc6e3d2564f9d03421
TSephanya Brun <tsbrun@icloud.com>
2020-12-02 14:59:25 -0500

    commit tree 3665d2f25b5fbf476c67ba12af7e73ef26c99526

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,22 @@
 class Song < ActiveRecord::Base
-  # add associations here
+  has_many :notes
+  belongs_to :artist 
+  belongs_to :genre
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+    # only persists relationship on child side 
+  end
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+    # if no artist set for song, returns nil instead of throwing an error
+  end
+
+  def song_notes=(notes)
+    notes.each do |content|
+      note = Note.create(content: content)
+      self.notes << note 
+    end
+  end
 end
